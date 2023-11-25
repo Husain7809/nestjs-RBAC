e06c95d12cf8622e3987aa6ead47470e69224624
Mohammad Husain <mh.varaliya@gmail.com>
2023-11-25 11:34:46 +0530

    added basic setup project

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,14 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { typeOrmConfig } from './config/mysql.config';
+import { AdminModule } from './api/admin/admin.module';
+import { ModuleModule } from './api/module/module.module';
 
 @Module({
-  imports: []
+  imports: [
+    TypeOrmModule.forRoot(typeOrmConfig),
+    AdminModule,
+    ModuleModule
+  ],
 })
 export class AppModule {}
